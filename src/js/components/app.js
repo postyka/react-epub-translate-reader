48a6306ab8a0d161c14c913d90eb7a8645730060
Denis Postika <den.postika@gmail.com>
2017-09-17 22:56:47 +0300

    Add image header and menu colors

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -11,10 +11,20 @@ class App extends Component {
 					<li className="app__header--logo">
 						<i className="fa fa-bookmark-o"></i>
 					</li>
-					<li><a href="#">HOME</a></li>
+					<li><a href="#"><i className="fa fa-circle-o"></i>HOME</a></li>
 					<li><a href="#">BOOKS</a></li>
-					<li><a href="#">DICTIONARY</a></li>
+					<li><a href="#">MY DICTIONARY</a></li>
 				</ul>
+				<div className="app__body">
+					<div className="app__body__book-header">
+						<div className="app__body__book-header--image" >
+							<img src="http://img-fotki.yandex.ru/get/9836/89721587.22c/0_d3cca_d5219616_XXXL.jpg"/>
+						</div>
+						<div className="app__body__book-header--info">
+							INFO
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
