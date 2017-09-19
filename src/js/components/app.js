a9fcbe41a074f785a4a7ac374c5a9867e19204c2
den.postika <den.postika@gmail.com>
2017-09-19 17:16:47 +0300

    Add Dimas recomendations

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -7,23 +7,23 @@ class App extends Component {
 	render() {
 		return (
 			<div className="app">
-				<ul className="app__header">
-					<li className="app__header--logo">
+				<header className="header">
+					<div className="header__logo">
 						<i className="fa fa-bookmark-o"></i>
-					</li>
-					<li><a href="#"><i className="fa fa-circle-o"></i>HOME</a></li>
-					<li><a href="#">BOOKS</a></li>
-					<li><a href="#">MY DICTIONARY</a></li>
-				</ul>
-				<div className="app__body">
-					<div className="app__body__book-header">
-						<div className="app__body__book-header--image" >
-							<img src="http://img-fotki.yandex.ru/get/9836/89721587.22c/0_d3cca_d5219616_XXXL.jpg"/>
-						</div>
-						<div className="app__body__book-header--info">
+					</div>
+					<ul className="header__nav">
+						<li><a href="#"><i className="fa fa-circle-o"></i>HOME</a></li>
+						<li><a href="#">BOOKS</a></li>
+						<li><a href="#">MY DICTIONARY</a></li>
+					</ul>
+				</header>
+				<div className="content">
+					<section className="hero">
+						<img src="http://img-fotki.yandex.ru/get/9836/89721587.22c/0_d3cca_d5219616_XXXL.jpg" />
+						<div className="hero__content">
 							INFO
 						</div>
-					</div>
+					</section>
 				</div>
 			</div>
 		)
